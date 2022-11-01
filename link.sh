2d3e40ee3b1e15baec6a719e469ea037fe986edc
Blechlawine <marczinser@gmx.de>
2022-11-01 12:50:44 +0100

    link custom user config for astro-nvim

--- link.sh
+++ link.sh
@@ -6,6 +6,7 @@ ln -s $(pwd)/.zshrc ~/.zshrc
 ln -s $(pwd)/.p10k.zsh ~/.p10k.zsh
 # Create links to nvim config files
 ln -s $(pwd)/apps/nvim/astro-nvim ~/.config/nvim
+ln -s $(pwd)/apps/nvim/custom/extend-astro-nvim $(pwd)/apps/nvim/astro-nvim/lua/user
 
 # Set default shell to zsh
 sudo chsh -s $(which zsh) $(whoami)
