b10e5924271181295f242bb3f4d5d6e91458abc4
Blechlawine <marczinser@gmx.de>
2022-12-20 10:53:14 +0100

    configure wilder

--- apps/nvim/custom/extend-astro-nvim/init.lua
+++ apps/nvim/custom/extend-astro-nvim/init.lua
@@ -246,9 +246,31 @@ local config = {
             -- },
 
             ["gelguy/wilder.nvim"] = {
-                -- TODO: configure wilder, this doesnt work yet
-                modes = { ":", "/", "?" }
-            }
+                config = function()
+                    -- config goes here
+                    local wilder = require("wilder")
+                    wilder.setup({
+                        modes = { ":", "/", "?" }
+                    })
+                    wilder.set_option("pipeline", {
+                        wilder.branch(
+                            wilder.cmdline_pipeline(),
+                            wilder.search_pipeline()
+                        ),
+                    })
+                    wilder.set_option('renderer', wilder.popupmenu_renderer(
+                        wilder.popupmenu_palette_theme({
+                            -- 'single', 'double', 'rounded' or 'solid'
+                            -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+                            border = 'rounded',
+                            max_height = '75%', -- max height of the palette
+                            min_height = 0, -- set to the same as 'max_height' for a fixed height window
+                            prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
+                            reverse = 0, -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+                        })
+                    ))
+                end
+            },
         },
         -- All other entries override the require("<key>").setup({...}) call for default plugins
         ["null-ls"] = function(config) -- overrides `require("null-ls").setup(config)`
