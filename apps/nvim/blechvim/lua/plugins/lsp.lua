e7d5c69df12e7f4005dcbf0742a34c6fdc57c717
Blechlawine <marczinser@gmx.de>
2023-09-23 20:05:04 +0200

    feat: use fallback typescript ls if local one is not found

--- apps/nvim/blechvim/lua/plugins/lsp.lua
+++ apps/nvim/blechvim/lua/plugins/lsp.lua
@@ -10,9 +10,33 @@ return {
             init = function()
                 require("core.utils").lazy_load_plugin("nvim-lspconfig")
             end,
-            opts = function()
-            end,
+            opts = {},
             config = function()
+                -- fallback to global typescript installation when local one is not found
+                local util = require 'lspconfig.util'
+                local function get_typescript_server_path(root_dir)
+                    local global_ts = '/home/[yourusernamehere]/.npm/lib/node_modules/typescript/lib'
+                    -- Alternative location if installed as root:
+                    -- local global_ts = '/usr/local/lib/node_modules/typescript/lib'
+                    local found_ts = ''
+                    local function check_dir(path)
+                        found_ts = util.path.join(path, 'node_modules', 'typescript', 'lib')
+                        if util.path.exists(found_ts) then
+                            return path
+                        end
+                    end
+                    if util.search_ancestors(root_dir, check_dir) then
+                        return found_ts
+                    else
+                        return global_ts
+                    end
+                end
+
+                require 'lspconfig'.volar.setup {
+                    on_new_config = function(new_config, new_root_dir)
+                        new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
+                    end,
+                }
             end,
         },
         require("plugins.mason"),
