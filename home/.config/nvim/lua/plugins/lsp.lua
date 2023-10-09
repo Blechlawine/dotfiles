c9aec246331ee10b4c44a752c67e4bcb4a77bbea
Blechlawine <marczinser@gmx.de>
2023-10-09 10:51:05 +0200

    fix(nvim): lsp mappings

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -59,11 +59,6 @@ return {
             event = "BufRead",
             config = function()
                 require("lsp_signature").setup()
-                vim.api.nvim_create_autocmd("BufWritePre", {
-                    callback = function()
-                        vim.lsp.buf.format()
-                    end
-                })
             end,
         },
     },
@@ -104,13 +99,13 @@ return {
             local options = { buffer = bufnr, remap = false }
             vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, options)
             vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, options)
-            vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, options)
-            vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, options)
+            vim.keymap.set("n", "<leader>lws", function() vim.lsp.buf.workspace_symbol() end, options)
+            vim.keymap.set("n", "<leader>ld", function() vim.diagnostic.open_float() end, options)
             vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, options)
             vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, options)
-            vim.keymap.set("n", "<leader>vca", function() vim.lsp.buf.code_action() end, options)
-            vim.keymap.set("n", "<leader>vrr", function() vim.lsp.buf.references() end, options)
-            vim.keymap.set("n", "<leader>vrn", function() vim.lsp.buf.rename() end, options)
+            vim.keymap.set("n", "<leader>la", function() vim.lsp.buf.code_action() end, options)
+            vim.keymap.set("n", "<leader>lu", function() vim.lsp.buf.references() end, options)
+            vim.keymap.set("n", "<leader>lr", function() vim.lsp.buf.rename() end, options)
             vim.keymap.set("i", "<C-h>", function() vim.lsp.buf.signature_help() end, options)
         end)
         lsp.setup()
