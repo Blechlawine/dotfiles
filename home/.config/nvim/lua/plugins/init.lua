1150352c1569c66abbc668de7f1e9582663ad582
Blechlawine <marczinser@gmx.de>
2023-10-09 10:49:41 +0200

    feat(nvim): add conform.nvim for better control over used formatters

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -55,6 +55,26 @@ local plugins = {
     require("plugins.lsp"),
 
     {
+        "stevearc/conform.nvim",
+        lazy = false,
+        opts = {
+            formatters_by_ft = {
+                javascript = { "biome" },
+                typescript = { "biome" },
+                json = { "biome" },
+                lua = { "stylua" }
+            },
+            format_on_save = {
+                timeout_ms = 1000,
+                lsp_fallback = true,
+            },
+        },
+        config = function(_, opts)
+            require("conform").setup(opts)
+        end,
+    },
+
+    {
         "tpope/vim-fugitive",
         cmd = {
             "G",
