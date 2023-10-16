a5180579b516b25266423ddf79c3d67987c623f6
Blechlawine <marczinser@gmx.de>
2023-10-16 08:22:22 +0200

    feat: try to enable projectDiagnostics in tsserver

--- home/.config/nvim/lua/plugins/lsp.lua
+++ home/.config/nvim/lua/plugins/lsp.lua
@@ -32,11 +32,18 @@ return {
                     end
                 end
 
-                require 'lspconfig'.volar.setup {
+                local lspconfig = require("lspconfig")
+                lspconfig.volar.setup({
                     on_new_config = function(new_config, new_root_dir)
                         new_config.init_options.typescript.tsdk = get_typescript_server_path(new_root_dir)
                     end,
-                }
+                })
+
+                lspconfig.tsserver.setup({
+                    experimental = {
+                        enable_project_diagnostics = true,
+                    },
+                })
             end,
         },
         require("plugins.mason"),
