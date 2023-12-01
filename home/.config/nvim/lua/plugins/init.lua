5e653104be1332b8fb227530889feff0556617b4
Blechlawine <marczinser@gmx.de>
2023-12-01 22:11:16 +0100

    feat: italics and catppuccin tmux config

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -22,7 +22,23 @@ local plugins = {
         priority = 1000,
         lazy = false,
         init = function()
-            require("onedarkpro").setup({})
+            require("onedarkpro").setup({
+                styles = {
+                    types = "NONE",
+                    methods = "NONE",
+                    numbers = "NONE",
+                    strings = "NONE",
+                    comments = "italic",
+                    keywords = "bold,italic",
+                    constants = "NONE",
+                    functions = "italic",
+                    operators = "NONE",
+                    variables = "NONE",
+                    parameters = "NONE",
+                    conditionals = "italic",
+                    virtual_text = "NONE",
+                }
+            })
             vim.cmd("colorscheme onedark_dark")
         end,
     },
