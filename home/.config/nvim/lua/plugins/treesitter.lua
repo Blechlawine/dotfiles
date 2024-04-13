36d3ee37a176fc5ca750ac6fb78dfaea122f0961
Marc <marczinser@gmx.de>
2024-04-13 19:12:04 +0200

    fix: treesitter not loading for some reason

--- home/.config/nvim/lua/plugins/treesitter.lua
+++ home/.config/nvim/lua/plugins/treesitter.lua
@@ -1,6 +1,6 @@
 return {
     "nvim-treesitter/nvim-treesitter",
-    cmd = { "TSInstall", "TSBufEnable", "TSBufDisable", "TSModuleInfo" },
+    lazy = false,
     build = ":TSUpdate",
     opts = {
         ensure_installed = {
