be7fe15a9555b7d1b680152a9e182581448c5963
Marc <marczinser@gmx.de>
2024-04-14 18:51:36 +0200

    feat: move git plugins into separate file and add lazygit to try out

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -144,52 +144,7 @@ return {
         end,
     },
 
-    -- GIT
-    {
-        "tpope/vim-fugitive",
-        cmd = {
-            "G",
-            "Git"
-        },
-    },
-    -- Adds git related signs to the gutter, as well as utilities for managing changes
-    {
-        'lewis6991/gitsigns.nvim',
-        event = "BufEnter",
-        opts = {
-            on_attach = function(bufnr)
-                local gitsigns = require('gitsigns')
-
-                local function map(mode, l, r, opts)
-                    opts = opts or {}
-                    opts.buffer = bufnr
-                    vim.keymap.set(mode, l, r, opts)
-                end
-
-                -- Navigation (jumping between hunks)
-                map("n", "]g", function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ "]g", bang = true })
-                    else
-                        gitsigns.nav_hunk("next")
-                    end
-                end, { desc = "Jump to next change" })
-
-                map("n", "[g", function()
-                    if vim.wo.diff then
-                        vim.cmd.normal({ "[g", bang = true })
-                    else
-                        gitsigns.nav_hunk("prev")
-                    end
-                end, { desc = "Jump to previous change" })
-
-                -- Actions
-                -- map("n", "<leader>gd", gitsigns.diffthis)
-                -- map("n", "<leader>gD", function() gitsigns.diffthis("~") end)
-                map("n", "<leader>gp", gitsigns.preview_hunk, { desc = "Preview hunk" })
-            end
-        },
-    },
+    require("plugins.git"),
 
     -- For TSX syntax highlighting
     {
