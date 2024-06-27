24fff5170e1e9c77c4a5ddaabd45eebb66d6ad49
Blechlawine <marczinser@gmx.de>
2024-06-27 14:51:46 +0200

    fix: only activate vue tsserver plugin in vue files

--- home/.config/nvim/lua/plugins/lspconfig.lua
+++ home/.config/nvim/lua/plugins/lspconfig.lua
@@ -179,7 +179,7 @@ return {
                             name = "@vue/typescript-plugin",
                             location = require("mason-registry").get_package("vue-language-server"):get_install_path() ..
                                 "/node_modules/@vue/language-server",
-                            languages = { "vue", "typescript", "javascript" },
+                            languages = { "vue" },
                         },
                     },
                 },
