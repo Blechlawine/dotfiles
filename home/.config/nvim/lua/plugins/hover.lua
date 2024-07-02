f7b02614d5a3d5f1775839be7423dda911ce58c1
Blechlawine <marczinser@gmx.de>
2024-07-02 11:36:19 +0200

    fix: pressing K twice now switches to the hover window

--- home/.config/nvim/lua/plugins/hover.lua
+++ home/.config/nvim/lua/plugins/hover.lua
@@ -59,9 +59,16 @@ return {
                 -- require('hover.providers.gh_user')
                 -- require('hover.providers.jira')
                 require('hover.providers.man')
-                require('hover.providers.dictionary')
+                -- require('hover.providers.dictionary')
             end,
         })
-        vim.keymap.set("n", "K", require("hover").hover, { desc = "hover.nvim" })
+        vim.keymap.set("n", "K", function()
+            local hover_win = vim.b.hover_preview
+            if hover_win and vim.api.nvim_win_is_valid(hover_win) then
+                vim.api.nvim_set_current_win(hover_win)
+            else
+                require("hover").hover()
+            end
+        end, { desc = "hover.nvim" })
     end,
 }
