c13bf63ba99bf52550596942372b94caa332455d
Blechlawine <marczinser@gmx.de>
2024-10-17 09:39:15 +0200

    fix: mini.surround not triggering on 's'
    this was apparently a problem in an update of which-key, which is why i
    removed the timeoutlen setting. i don't like this fix tho

--- home/.config/nvim/lua/core/mappings.lua
+++ home/.config/nvim/lua/core/mappings.lua
@@ -16,7 +16,7 @@ end, { desc = "Expand Emmet completion" })
 ---NORMAL MODE-------------------------------------------------------------------------------------------------------------------
 vim.keymap.set("n", "<leader>t", " Terminal")
 vim.keymap.set("n", "<leader>g", "󰊢 Git")
-vim.keymap.set("n", "<leader>s", "󱂬 Sessions")
+vim.keymap.set({ 'n', 'x' }, 's', '<Nop>')
 
 -- Easy paste for system clipboard
 vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
