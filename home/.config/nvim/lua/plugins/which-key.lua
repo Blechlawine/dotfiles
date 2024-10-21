d386f8b9dfd1785517ec561b87e4773b2c771fd5
Blechlawine <marczinser@gmx.de>
2024-10-21 11:16:23 +0200

    fix: either which-key or mini.surround not working correctly, now they both seem to work

--- home/.config/nvim/lua/plugins/which-key.lua
+++ home/.config/nvim/lua/plugins/which-key.lua
@@ -1,9 +1,9 @@
 return {
     "folke/which-key.nvim",
-    event = "VeryLazy",
+    lazy = false,
     init = function()
-        vim.o.timeout = true
-        -- vim.o.timeoutlen = 0
+        vim.o.timeout = false
+        vim.o.timeoutlen = 0
     end,
     opts = {},
 }
