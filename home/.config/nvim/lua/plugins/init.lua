337c98c7c42dae7ff226e390c497c03901f5d573
Blechlawine <marczinser@gmx.de>
2025-03-28 13:13:59 +0100

    fix: activate lsp_fallback for formatting again

--- home/.config/nvim/lua/plugins/init.lua
+++ home/.config/nvim/lua/plugins/init.lua
@@ -106,7 +106,7 @@ return {
             },
             format_on_save = {
                 timeout_ms = 1000,
-                lsp_fallback = false,
+                lsp_fallback = true,
             },
         },
         config = function(_, opts)
